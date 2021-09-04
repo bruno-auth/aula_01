12365ca000d2fc72da3335284e6c0a5e0a7c55c6
Bruno Auth <brunoauth10@gmail.com>
2021-09-04 09:22:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para calcular a área de um terreno, digite 1!");
+            int teste = Convert.ToInt32(Console.ReadLine());
+            if (teste == 1)
+            {
+                calcularArea();
+            }
+            else
+            {
+                Console.WriteLine("Então suma daqui!");
+            }
+            
+        }
+
+        static void calcularArea()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Calcular área de terreno!");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Informe a largura do terreno:");
+            int largura = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe o comprimento do terreno:");
+            int comprimento = Convert.ToInt32(Console.ReadLine());
+            int resultado = largura * comprimento;
+            Console.WriteLine("A área do terreno é: " + resultado + "m²!");
+            Console.WriteLine(@$"A área do terreno é: {resultado}m²!");
+            Console.WriteLine(String.Format(@"A área do terreno é: {0}m²!", resultado));
+        }
+
+
+    }
+}
